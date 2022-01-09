2c4ebce83b58fb3744733dd54fc34c55795c0e0f
techcoderunner <londhesachin6@gmail.com>
2022-01-09 12:12:33 +0530

    cdk synth fix

--- cdk/app.py
+++ cdk/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+from aws_cdk import core
 from app.app_stack import AppStack
 
 
--- cdk/app/app_stack.py
+++ cdk/app/app_stack.py
@@ -1,23 +1,18 @@
-from aws_cdk import (
-    core,
-    aws_iam as iam,
-    aws_s3 as s3,
-    aws_s3_deployment as s3deploy
-)
+from aws_cdk import core
 
-from static_site_hosting import StaticSiteHosting
+from app.static_web_hosting_stack import StaticWebHostingStack
 
+class AppStack:
 
-class StaticWebHostingStack(core.Stack):
+    def __init__(self):
+        self.app = core.App()
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+    def build(self) -> core.App:
 
-        static_web_hosting = StaticSiteHosting(
-            self, "techcoderunner_static_web", bucket_name="techcoderunner-static-website")
+        StaticWebHostingStack(
+            self.app,
+            "static-web-hosting-stack",
+            env={'region':'us-east-1'}
+        )
 
-        s3deploy.BucketDeployment(self, "DeployWebsite",
-                                  sources=[
-                                      s3deploy.Source.asset("webContent")],
-                                  destination_bucket=static_web_hosting.bucket
-                                  )
+        return self.app
